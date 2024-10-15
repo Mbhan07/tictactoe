a4985bebb6954879ae95327dbb2ef0492afb2fa6
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-10-15 10:02:09 -0700

    Add files via upload
    no

--- ttt.cpp
+++ ttt.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+
+
+//print the board
 void printBoard(int board[3][3]){
   int BLANK = 0;
   int X_MOVE = 1;
@@ -13,14 +16,15 @@ void printBoard(int board[3][3]){
   int X_WINS = 0;
   int O_WINS = 0;
 
+  //prints the numbered columns
   cout << "\t1\t2\t3\n" << endl;
-  for (int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++){ //iterate through a two dimensional array
     char letter = (char)('a' + i); //my mom said to do this so i did it!
-    cout << letter << "\t";
+    cout << letter << "\t"; // output the letter rows
     for (int j = 0; j < 3; j++) {
-      if(board[i][j] == BLANK){
-	cout << "\t";
-      }else if(board[i][j] == X_MOVE){
+      if(board[i][j] == BLANK){ // if board is blank, indent
+	cout << "\t"; 
+      }else if(board[i][j] == X_MOVE){ // if it is X move, print X
 	cout << "X\t";
       } else if(board[i][j] == O_MOVE){
 	cout << "O\t";
@@ -33,8 +37,10 @@ void printBoard(int board[3][3]){
 
 }
 
+//to clear the board of any moves
 void clearBoard(int board[3][3]){
   int BLANK = 0;
+  //iteratre through two dimensional array and every coordinate should be blank
   for (int row = 0; row < 3; row++){
     for (int column = 0; column < 3; column++){
       board[row][column] = BLANK;
@@ -42,20 +48,21 @@ void clearBoard(int board[3][3]){
   }
 }
 
-
+//check for ties by seeing if every spot is filled
 bool checkTie(int board[3][3]){
   int BLANK = 0;
   for(int row = 0; row < 3; row++){
     for (int column = 0; column < 3; column++){
       if(board[row][column] == BLANK){
-	return false;
+	return false; // if any spot is not filled, tie is not true
       }
     }
   }
   return true;
 }
 
-
+//check for wins, diganoal, vertical, or horizontal
+//return true if the case is met, return false otherwise
 bool checkWin(int player, int board[3][3]){
 
   if (board[0][0] == player && board[0][1] == player && board[0][2] == player){
@@ -85,30 +92,35 @@ bool checkWin(int player, int board[3][3]){
     return false;
   }
 }
-
-void playAgain(int board[3][3], O_MOVE, X_MOVE){
+//prompts the user after a win,or tie
+void playAgain(int board[3][3], int X_MOVE, int O_MOVE){
   char ans[10];
+  int X_TURN = 0;
+  int turn = X_TURN;
   bool playing = true;
+  //if win, or tie is true, the following should happen
  if (checkWin(X_MOVE, board) == true || checkWin(O_MOVE, board) == true || checkTie(board) == true){
+   //ask user if they want to play again
+
          cout << "Do you want to play again? (yes or no)" << endl;
          cin >> ans;
-
+	 //use strcmp to determine if user entered yes or no
          if (strcmp(ans, "yes") == 0){
              clearBoard(board);
-             playing = true;
-         }else if (strcmp(ans, "no") == 0){
+             playing = true; // if yes, clear board and restart the loop
+         }else if (strcmp(ans, "no") == 0){ //else, thank them for their time and exit loop
           cout << "Thanks for playing!" << endl;
-          //      break;
-         }else {
+          exit(0);
+         }else { //ensures that they input the right thing
           cout << "Invalid input! Please enter 'yes' or 'no': " << endl;
          }
        }
-
-
-}  
+       }  
 
 
 int main(){
+
+  //necessary variables
   	int BLANK = 0;
 	int X_MOVE = 1;
 	int O_MOVE = 2;
@@ -118,92 +130,125 @@ int main(){
 	int O_WINS = 0;
 	int TIE = 0;
 	char blah[10];
-	int board[3][3];
+	int board[3][3]; // initialize gameboard
 	int turn = X_TURN;
 	bool playing = true;
 	char ans[10];
 	clearBoard(board);
-    while(playing){
-	  printBoard(board);
-	  cout << "\n" << endl;
+	while(playing){ //while the game is actively happening
+      int row;
+      int column;
+      printBoard(board); //print the board and check for tie first so that gameplay can flow smoothly
+      if(checkTie(board) == true){ // if there is a tie
+	    //cout <<"test" << endl;
+	printBoard(board); //print board, incrrement number of ties
+	    TIE++;
+	    cout << "It's a tie!" << endl; //inform user and print number of wins
+	    cout << "X wins: " << X_WINS << "\tO wins: " << O_WINS << "\tTies: " << TIE << endl;
+	    playAgain(board, X_MOVE, O_MOVE); //prompt user for next steps
+	    turn = X_TURN; //switch turn
+	  }
+	  
+      cout << "\n" << endl; //instruct user how to enter inputt
 	  cout << "Place your move by inputting a letter followed by a number (e.g. a1 or b2): " << endl;
 	  cin >> blah;
 	  cout << "\n";
 
-	  if (strlen(blah) != 2){
+	  if (strlen(blah) != 2){ // if length is more than two, prompt user to enter proper input
 	    cout << "Invalid input! Please enter a letter followed by a number (e.g. a1 or b2): " << endl;
 	  }
-	  else if (blah[0] == '1'){
-	    playing = false;
-	  }
-	  else if (blah[0] != 'a' && blah[0] != 'b' && blah [0] != 'c'){
+	  else if (blah[0] != 'a' && blah[0] != 'b' && blah [0] != 'c'){ // if the first character is not a letter
 	    cout << "Invalid input! The first character must be a letter (a, b, or c): \n" << endl;
 	  }
-	  else if (blah[1] != '1' && blah[1] != '2' && blah[2] != '3'){
+	  else if (blah[1] != '1' && blah[1] != '2' && blah[1] != '3'){ // if the second character is not a number
 	    cout << "Invalid input! The second character must be a number (1, 2, or 3): \n" << endl;
 	
 	  }
-	  else {
-	    int row = (char)(blah[0] - 'a');
-	    int column = (char)(blah[1] - '1');
-	    if(board[row][column] == BLANK){
-	      if(turn == X_TURN){
+	  else { // covert the letter/number input to coordinates on the game board
+	    if (strcmp(blah, "a1") == 0){
+	      row = 0;
+	      column = 0;
+	    }else if(strcmp(blah, "a2") == 0){
+	      row = 0;
+	      column = 1;
+	    }else if (strcmp(blah, "a3") == 0){
+	      row = 0;
+	      column = 2;
+	    }
+	    else if (strcmp(blah, "b1") == 0){
+	      row = 1;
+	      column = 0;
+	    }
+	    else if (strcmp(blah, "b2") == 0){
+	      row = 1;
+	      column = 1;
+	    }
+	    else if (strcmp(blah, "b3") == 0){
+	      row=  1;
+	      column = 2;
+	    }
+	    else if (strcmp(blah, "c1") == 0){
+	      row = 2;
+	      column = 0;
+	    }
+	    else if (strcmp(blah, "c2") == 0){
+	      row = 2;
+	      column = 1;
+	    }
+	    else if(strcmp(blah, "c3") == 0){
+	      row = 2;
+	      column = 2;
+	    }
+	    if(board[row][column] == BLANK){ // if the coordinate given is blank, 
+	      //cout << row << ", " << column << endl;
+	      if(turn == X_TURN){ // if it is X's turn, place X, then check for wins
 		board[row][column] = X_MOVE;
+		if (checkWin(X_MOVE, board) == true){// if true, 
+		  printBoard(board); //print the final game board, increment number of x wins, inform user, and ask if they want to play again
+		  X_WINS++;
+		  cout << "X wins! " << endl;
+		  cout << "X wins: " << X_WINS << "\t O Wins: "<< O_WINS << "\tTies: " << TIE << endl;
+		 playAgain(board, X_MOVE, O_MOVE);
+		 //cout <<"Theoretically, it should switch to X here." << endl;
+		 turn = X_TURN; // if win, turn should go back to x 
+		}else { // else, turn goes back to o
+		  //cout << "Is this why it's not starting with X?" << endl;
 		turn = O_TURN;
-	      } else {
+		}
+		
+	      } else if (turn == O_TURN){ // if it is O's turn, place O where they have given the coordinates
 		board[row][column] = O_MOVE;
-		turn = X_TURN;
+		// check if O has won
+		if(checkWin(O_MOVE, board)){
+		  //cout << "gaga" << endl; 
+		  printBoard(board); //print final board, increment O wins, inform user they won and prompt to play again
+		    O_WINS++;
+		    cout << "O won!"<< endl;
+		    cout << "X wins: " << X_WINS << "\t O Wins: "<< O_WINS << "\t Ties: " << TIE << endl;
+		    playAgain(board, X_MOVE, O_MOVE);
+		    turn = X_TURN; // switch turn
+		}else{
+		  //cout << "O should switch to X move after winning here." << endl;
+		  turn = X_TURN; // should switch to X normally, but this is outside of the win condition
+		}
 	      }
-	    } else {
+	    }else{ // if spot is not blank, tell user that there is already a piece there
 	      cout << "Invalid move! There is already a piece there \n" << row << ", " << column;
 	    }
 	  }
 
-	  if (checkWin(X_MOVE, board)){
-	    printBoard(board);
-	      X_WINS++;
-	      turn = X_TURN;
-	      cout << "\n X won! "<< endl;
-	      cout << "X wins: "<< X_WINS << "\t O Wins: " << O_WINS << "\t Ties: " << TIE << endl;
-	      clearBoard(board);
-	      playAgain(board, O_MOVE, X_MOVE);
-	   }else if(checkWin(O_MOVE, board)){
-		clearBoard(board);
-		O_WINS++;
-		turn = X_TURN;
-		cout << "\nO Won!\n";
-		cout << "\nX Wins: " << X_WINS << "\tO Wins: " << O_WINS << "\tTies: " << TIE << "\n\n";
-	
-	  }else if(checkTie(board)) {
-	    TIE++;
-	    clearBoard(board);
-	    turn = X_TURN;
-	    cout << "\nIt is a tie!\n";
-	    cout << "\nX Wins: " << X_WINS << "\tO Wins: " << O_WINS << "\tTies: " << TIE << "\n\n";
-	
-	  }
-    
-
-	  /*   if (checkWin(X_MOVE, board) == true || checkWin(O_MOVE, board) == true || checkTie(board) == true){
-	 cout << "Do you want to play again? (yes or no)" << endl;
-	 cin >> ans;
-
-	 if (strcmp(ans, "yes") == 0){
-	     clearBoard(board);
-	     playing = true;
-	 }else if (strcmp(ans, "no") == 0){
-	  cout << "Thanks for playing!" << endl;
-	  //	  break;
-	 }else {
-	  cout << "Invalid input! Please enter 'yes' or 'no': " << endl;
-	 }
-	 }*/
-    }    
+       }    
 return 0;
 
 }
 
+// thanks!
 
+
+//            > - <
+//          ( ^   ^ )
+//          (       )
+//            ^  ^
 
 
 
